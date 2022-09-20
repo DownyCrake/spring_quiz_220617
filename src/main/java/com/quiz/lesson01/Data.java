bc9f933579ad315d03e2d7a114a0935e27244910
DESKTOP-571HM4G\user <wishchaser@naver.com>
2022-09-20 19:45:43 +0900

    spring quiz 초기설정

---
+++ src/main/java/com/quiz/lesson01/Data.java
@@ -0,0 +1,28 @@
+package com.quiz.lesson01;
+
+public class Data {
+	private String title;
+	private String user;
+	private String contnet;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getContnet() {
+		return contnet;
+	}
+	public void setContnet(String contnet) {
+		this.contnet = contnet;
+	}
+	
+	
+}
